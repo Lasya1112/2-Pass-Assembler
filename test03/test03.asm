3b30571f0528503dc7076464a872724ad9593b47
Lasya Penugonda <165418884+Lasya1112@users.noreply.github.com>
2025-06-14 20:01:57 +0545

    Add files via upload

---
+++ test03/test03.asm
@@ -0,0 +1,10 @@
+; Name: Sai Lasya Penugonda
+; Roll: 2201CS61
+; This test file was provided for testing purposes.
+; I am not the author of this file
+; test3.asm
+; Test SET
+val: SET 75
+ldc     val
+adc     val2
+val2: SET 66
